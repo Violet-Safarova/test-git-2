5265224884cd7c974e80594c4d9a145d93a304b4
Violet Safarova <keomanivongvii@yandex.com>
2023-06-10 14:49:29 +0500

    made changes in master

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-console.log('Hello World!');
-
 console.error('Error');
 console.log('Hello');
+
+const userAge = 20;
+if(age >= 18) {
+    console.log("Вы соверленнолетний");
+} else {
+    console.log("Вы не совершеннолетний");
+}
